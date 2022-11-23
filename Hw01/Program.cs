c26f2f99a91fab73357bf763824e425d272d6c4d
ANDRBAL <ba.balandin@gmail.com>
2022-11-23 06:17:07 +0300

    Задача 1

--- Hw01/Program.cs
+++ Hw01/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ Console.WriteLine("Введите первое число: ");
+ int num1 = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите второе число: ");
+ int num2 = Convert.ToInt32(Console.ReadLine());
+ 
+ if(num1 > num2)
+ {
+ Console.WriteLine("Первое число больше второго");
+  }
+ else if (num1 < num2)
+ {
+Console.WriteLine("Первое число меньше второго");
+ }
+ else
+ {
+Console.WriteLine("Оба числа равны");
+ }
