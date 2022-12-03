72d8402116a4d7d4670423dcd6a240111b7c8631
ANDRBAL <ba.balandin@gmail.com>
2022-12-03 10:56:02 +0300

    done task

--- HW02/Program.cs
+++ HW02/Program.cs
@@ -1,12 +1,71 @@
-﻿Console.Write("Введи число: ");
-
-int a = Convert.ToInt32(Console.ReadLine());
-string b = Convert.ToString(a);
-
-if (b.Length > 2){
-  Console.WriteLine("3я цифра -> " + b[2]);
+﻿///Урок 3
+Console.WriteLine("\n Введите число");
+int x1 = Coordinate("x", "A");
+int y1 = Coordinate("y", "A");
+int z1 = Coordinate("z", "A");
+int x2 = Coordinate("x", "B");
+int y2 = Coordinate("y", "B");
+int z2 = Coordinate("z", "B");
+int Coordinate(string coorName, string pointName)
+{
+Console.Write($"Введите координату {coorName} точки {pointName}: ");
+return Convert.ToInt16(Console.ReadLine());
+}
+double Decision(double x1, double x2, 
+double y1, double y2, 
+double z1, double z2){
+return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+Math.Pow((y2-y1), 2) + 
+Math.Pow((z2-z1), 2));
 }
 
-else {
-  Console.WriteLine("-> 3я цифра отсутсвует");
-}
+double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+Console.WriteLine($"Длина отрезка  {segmentLength}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///Урок 2
+///Console.Write("Введи число: ");
+///
+///int a = Convert.ToInt32(Console.ReadLine());
+///string b = Convert.ToString(a);
+///
+///if (b.Length > 2){
+///  Console.WriteLine("3я цифра -> " + b[2]);
+///}
+///
+///else {
+///  Console.WriteLine("-> 3я цифра отсутсвует");
+///}
--- HW03/Program.cs
+++ HW03/Program.cs
@@ -1,11 +1,63 @@
-﻿Console.Write("Введи цифру, обозначающую день недели от 1 до 7: ");
-int day = Convert.ToInt32(Console.ReadLine());
+﻿///Урок 3
+Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+void Cube(int[] cube){
+int counter = 0;
+int length = cube.Length;
+while (counter <  length){
+cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+counter++;
+}
+}
+void PrintArry(int[] coll){
+int count = coll.Length;
+int index = 0;
+while(index < count){
+Console.Write(coll[index]+ " ");
+index++;
+}
+} 
+int[] arry = new int[cube+1];
+Cube(arry);
+PrintArry(arry);
 
-  if (day == 6 || day == 7) {
-  Console.WriteLine("это выходной день");
-  }
 
-    else Console.WriteLine("это будний день");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///Урок 2 
+///Console.Write("Введи цифру, обозначающую день недели от 1 до 7: ");
+///int day = Convert.ToInt32(Console.ReadLine());
+///
+///if (day == 6 || day == 7) {
+///  Console.WriteLine("это выходной день");
+///  }
+///
+///    else Console.WriteLine("это будний день");
 
 
 
