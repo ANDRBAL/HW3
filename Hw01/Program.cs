b4e01824a102b95a32c72c854484aab26cbfd970
ANDRBAL <ba.balandin@gmail.com>
2022-12-03 10:53:15 +0300

    Done task3

--- Hw01/Program.cs
+++ Hw01/Program.cs
@@ -1,6 +1,63 @@
-﻿Console.Write("Введи трёхзначное число: ");
+﻿///Урок 3
+Console.Write("Введите 5ти значное число: ");
+string? number = Console.ReadLine();
+void CheckingNumber(string number){
+if (number[0]==number[4] || number[1]==number[3]){
+Console.WriteLine($"Это число: {number} - палиндром.");
+}
+else Console.WriteLine($"Это число: {number} - Не палиндром.");
+}
+if (number!.Length == 5){
+CheckingNumber(number);
+}
+else Console.WriteLine($"Не правильное число");
 
-int a = Convert.ToInt32(Console.ReadLine());
-string b = Convert.ToString(a);
 
-Console.WriteLine("вторая цифра этого числа -> "+b[1]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///Урок 2
+///Console.Write("Введи трёхзначное число: ");
+///
+///int a = Convert.ToInt32(Console.ReadLine());
+///string b = Convert.ToString(a);
+///
+///Console.WriteLine("вторая цифра этого числа -> "+b[1]);
